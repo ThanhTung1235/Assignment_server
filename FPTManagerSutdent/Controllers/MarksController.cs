4644fccb86745875a9ad20ae11b97047481e7d2c
thjennhjenoxi <sondtd00591@fpt.edu.vn>
2018-12-23 13:32:39 +0700

    Create the controller and Delete the Form Status in Course/Create.cshtml

---
+++ FPTManagerSutdent/Controllers/MarksController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FPTManagerSutdent.Data;
+using FPTManagerSutdent.Models;
+
+namespace FPTManagerSutdent.Controllers
+{
+    public class MarksController : Controller
+    {
+        private readonly Datacontext _context;
+
+        public MarksController(Datacontext context)
+        {
+            _context = context;
+        }
+
+        // GET: Marks
+        public async Task<IActionResult> Index()
+        {
+            var datacontext = _context.Mark.Include(m => m.Course);
+            return View(await datacontext.ToListAsync());
+        }
+
+        // GET: Marks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mark = await _context.Mark
+                .Include(m => m.Course)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mark == null)
+            {
+                return NotFound();
+            }
+
+            return View(mark);
+        }
+
+        // GET: Marks/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Course, "Id", "Id");
+            return View();
+        }
+
+        // POST: Marks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Theory,Practice,Assignment,CreatedAt,UpdateAt,CourseId,StudentId")] Mark mark)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mark);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Course, "Id", "Id", mark.CourseId);
+            return View(mark);
+        }
+
+        // GET: Marks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mark = await _context.Mark.FindAsync(id);
+            if (mark == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseId"] = new SelectList(_context.Course, "Id", "Id", mark.CourseId);
+            return View(mark);
+        }
+
+        // POST: Marks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Theory,Practice,Assignment,CreatedAt,UpdateAt,CourseId,StudentId")] Mark mark)
+        {
+            if (id != mark.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mark);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MarkExists(mark.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Course, "Id", "Id", mark.CourseId);
+            return View(mark);
+        }
+
+        // GET: Marks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mark = await _context.Mark
+                .Include(m => m.Course)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mark == null)
+            {
+                return NotFound();
+            }
+
+            return View(mark);
+        }
+
+        // POST: Marks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mark = await _context.Mark.FindAsync(id);
+            _context.Mark.Remove(mark);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MarkExists(int id)
+        {
+            return _context.Mark.Any(e => e.Id == id);
+        }
+    }
+}
