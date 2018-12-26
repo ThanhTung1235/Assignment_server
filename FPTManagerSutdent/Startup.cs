b825cc0c7e633658ed9cce192f22ef4395c36be8
ok <tuandpd00617@fpt.edu.vn>
2018-12-26 15:35:22 +0700

    sua-hien-thi

--- FPTManagerSutdent/Startup.cs
+++ FPTManagerSutdent/Startup.cs
@@ -60,7 +60,7 @@ namespace FPTManagerSutdent
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=ClassRooms}/{action=Index}/{id?}");
             });
         }
     }
